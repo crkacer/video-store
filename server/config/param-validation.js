a2503b36384c6f4145884dbfb8dae64085dd6a48
Duc Nguyen <minhducng93@gmail.com>
2018-11-10 22:41:50 -0500

    Modify params validation and video controller

---
+++ server/config/param-validation.js
@@ -0,0 +1,76 @@
+const Joi = require('joi');
+const Regex = require('./regex');
+
+module.exports = {
+  // POST /api/user
+  createUser: {
+    body: {
+      firstname: Joi.string().required(),
+      lastname: Joi.string().required(),
+      username: Joi.string().required(),
+      password: Joi.string().required(),
+      mobileNumber: Joi.string().regex(Regex.mobile).required()
+    }
+  },
+
+  // UPDATE /api/user/:userId
+  updateUser: {
+    body: {
+      username: Joi.string().required(),
+      mobileNumber: Joi.string().regex(Regex.mobile).required()
+    },
+    params: {
+      userId: Joi.string().hex().required()
+    }
+  },
+  // POST /api/user/profile
+  getUserByUsername: {
+    body: {
+      username: Joi.string().required()
+    }
+  },
+
+  // POST /api/auth/login
+  login: {
+    body: {
+      username: Joi.string().required(),
+      password: Joi.string().required()
+    }
+  },
+
+  // POST /api/video
+  createVideo: {
+    body: {
+      title: Joi.string().required(),
+      image: Joi.string().required(),
+      description: Joi.string(),
+      genre: Joi.string().required(),
+      director: Joi.string().required(),
+      status: Joi.string().required(),
+      star: Joi.string().regex(Regex.rating).required(),
+      length: Joi.number().required()
+    }
+  },
+
+  // PUT /api/video/:id
+  updateVideo: {
+    body: {
+      title: Joi.string().required(),
+      image: Joi.string().required(),
+      description: Joi.string(),
+      genre: Joi.string().required(),
+      director: Joi.string().required(),
+      status: Joi.string().required(),
+      star: Joi.string().regex(Regex.rating).required(),
+      length: Joi.number().required()
+    }
+  },
+
+  // DELETE /api/video/:id
+  deleteVideo: {
+    body: {
+      id: Joi.string().required()
+    }
+  },
+
+};
---
+++ server/src/video/video.controller.js
@@ -0,0 +1,79 @@
+const Video = require('./video.model');
+const User = require('../user/user.model');
+const bcrypt = require('bcrypt');
+
+function load(req, res, next) {
+  const indx = req.params.id;
+  return Video.get(indx)
+    .then(video => {
+      req.video = video;
+      return res.json(video);
+    })
+    .catch(e => next(e));
+}
+
+function create(req, res, next) {
+  const params = req.body;
+  const video = new Video({
+      title: params.title,
+      image: params.image,
+      genre: params.genre,
+      director: params.director,
+      status: params.status,
+      star: params.star,
+      length: params.length,
+      description: params.description
+  });
+
+video.save()
+  .then(savedVideo => res.json(savedVideo))
+  .catch(e => next(e));
+}
+
+function update(req, res, next) {
+  const {id} = req.params;
+  Video.get(id)
+  .then(video => {
+    video.title = req.body.title;
+    video.image = req.body.image;
+    video.description = req.body.description;
+    video.genre = req.body.genre;
+    video.director = req.body.director;
+    video.status = req.body.status;
+    video.star = req.body.star;
+    video.length = req.body.length;
+    video.save()
+      .then(savedVideo => res.json(savedVideo))
+      .catch(e => next(e));
+  })
+  .catch(e => next(e));
+  
+}
+
+/**
+ * Get video list.
+ * @property {number} req.query.skip 
+ * @property {number} req.query.limit 
+ * @returns {Video[]}
+ */
+function list(req, res, next) {
+  const { limit = 50, skip = 0 } = req.query;
+  Video.list({ limit, skip })
+    .then(videos => res.json(videos))
+    .catch(e => next(e));
+}
+
+/**
+ * Delete video.
+ * @returns {Video}
+ */
+function remove(req, res, next) {
+  const id = req.params.id;
+  Video.remove(id)
+    .then(deletedVideo => res.json(deletedVideo))
+    .catch(e => next(e));
+}
+
+
+
+module.exports = { load, create, update, list, remove };
