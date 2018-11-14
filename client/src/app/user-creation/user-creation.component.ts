c2216808ab6b7ee78c5752668d1870465edaa10b
Duc Nguyen <minhducng93@gmail.com>
2018-11-14 14:50:30 -0500

    Add User Creation component

---
+++ client/src/app/user-creation/user-creation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'user-create',
+  templateUrl: './user-creation.component.html',
+  styleUrls: ['./user-creation.component.css']
+})
+export class UserCreationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
